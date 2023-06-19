7f1582cac0d4c11055f294841fc3a534375f409c
ShoryaThakur <thakurshorya417@gmail.com>
2023-06-19 17:03:47 +0530

    updated

--- Js/script.js
+++ Js/script.js
@@ -1,5 +1,10 @@
-$(function(){
-    $(".fa-navicon").click(function(){
-        $("ul").slideToggle();
-    })
-})
+$(function () {
+  $(".fa-navicon").click(function () {
+    $("ul").slideToggle();
+  });
+  $(".center").fadeOut(500);
+  $(".center").fadeIn(1000);
+});
+function refresh(){
+    location.reload();
+}
