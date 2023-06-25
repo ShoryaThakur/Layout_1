98a4bd250d49ec0668bb07d8160dbdc83e4bf882
ShoryaThakur <thakurshorya417@gmail.com>
2023-06-25 23:59:34 +0530

    final

--- Js/script.js
+++ Js/script.js
@@ -2,8 +2,8 @@ $(function () {
   $(".fa-navicon").click(function () {
     $("ul").slideToggle();
   });
-  // $(".center").fadeOut(500);
-  // $(".center").fadeIn(1000);
+  $(".center").fadeOut(500);
+  $(".center").fadeIn(1000);
 });
 function refresh(){
     location.reload();
